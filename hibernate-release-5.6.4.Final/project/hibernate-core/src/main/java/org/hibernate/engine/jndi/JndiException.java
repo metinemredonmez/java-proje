849581732798140d6e824e1e35e7496a4851a6b0
metinemredonmez <metinemredonmez@hotmail.com>
2022-02-10 21:50:33 +0300

    Initial commit

---
+++ hibernate-release-5.6.4.Final/project/hibernate-core/src/main/java/org/hibernate/engine/jndi/JndiException.java
@@ -0,0 +1,35 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.engine.jndi;
+import org.hibernate.HibernateException;
+
+/**
+ * An exception indicating trouble accessing JNDI
+ *
+ * @author Steve Ebersole
+ */
+public class JndiException extends HibernateException {
+	/**
+	 * Constructs a JndiException
+	 *
+	 * @param message Message explaining the exception condition
+	 * @param cause The underlying cause
+	 */
+	public JndiException(String message, Throwable cause) {
+		super( message, cause );
+	}
+
+	/**
+	 * Constructs a JndiException
+	 *
+	 * @param message Message explaining the exception condition
+	 */
+	public JndiException(String message) {
+		super( message );
+	}
+
+}
